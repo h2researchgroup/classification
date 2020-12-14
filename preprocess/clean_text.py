ef63aa0440491af560ac62557bbae1306f75d493
Jaren Haber <jhaber@berkeley.edu>
2020-12-14 11:15:25 -0600

    new fn for Apache tokenizing, cleaned up old functions

--- preprocess/clean_text.py
+++ preprocess/clean_text.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Author: Jaren Haber, PhD Candidate
-# Institution (as of this writing): University of California, Berkeley, Dept. of Sociology
-# Date created: Fall 2018
-# Date last modified: June 6, 2019 
-# Source GitHub repo: https://github.com/jhaber-zz/data_management/
-# Modified for: https://github.com/h2researchgroup/Computational-Analysis-For-Social-Science
-# Description: Essential functions for text preprocessing, e.g. as in training word embeddings or topic models. The core function cleans sentences by removing stopwords, etc.; elementary functions create lists of stopwords, punctuation, and unicode; a wish-list function gathers most common words from a text corpus.
+'''
+@title: General Functions to Preprocess Academic Articles
+@author: Jaren Haber, PhD, Georgetown University
+@coauthors: Prof. Heather Haveman, UC Berkeley; Yoon Sung Hong, Wayfair
+@contact: Jaren.Haber@georgetown.edu
+@project: Computational Literature Review of Organizational Scholarship
+@repo: https://github.com/h2researchgroup/classification/
+@date_created: Fall 2018
+@date_modified: December 14, 2020
+@description: Essential functions for text preprocessing. The core function cleans sentences by removing stopwords, proper nouns, etc. Other functions gather proper nouns and create lists of stopwords, punctuation, and unicode.
+'''
 
 # Import packages
 import re, datetime
@@ -37,10 +41,11 @@ from java.io import StringReader
 from org.apache.lucene.analysis.ja import JapaneseAnalyzer
 from org.apache.lucene.analysis.standard import StandardAnalyzer, StandardTokenizer
 from org.apache.lucene.analysis.tokenattributes import CharTermAttribute
-lucene.initVM(vmargs=['-Djava.awt.headless=true'])
 
-def stopwords_make(vocab_path_old = "", extend_stopwords = False):
+
+def stopwords_make(extend_stopwords = False):
     """Create stopwords list. 
+    
     If extend_stopwords is True, create larger stopword list by joining sklearn list to NLTK list."""
                                                      
     stop_word_list = list(set(stopwords.words("english"))) # list of english stopwords
@@ -52,69 +57,10 @@ def stopwords_make(vocab_path_old = "", extend_stopwords = False):
         stop_word_list.append((datetime.date(2008, i, 1).strftime('%B')).lower())
     for i in range(1, 2100):
         stop_word_list.append(str(i))
-
-    # Add other common stopwords
-    stop_word_list.append('00') 
-    stop_word_list.extend(['mr', 'mrs', 'sa', 'fax', 'email', 'phone', 'am', 'pm', 'org', 'com', 
-                           'Menu', 'Contact Us', 'Facebook', 'Calendar', 'Lunch', 'Breakfast', 
-                           'facebook', 'FAQs', 'FAQ', 'faq', 'faqs', '1', '2', '3', '4', '5', '6',
-                          '7','8','9','0']) # web stopwords + numbers
-    stop_word_list.extend(['el', 'en', 'la', 'los', 'para', 'las', 'san']) # Spanish stopwords
-    stop_word_list.extend(['angeles', 'diego', 'harlem', 'bronx', 'austin', 'antonio']) # cities with many charter schools
-
-    # Add state names & abbreviations (both uppercase and lowercase) to stopwords
-    states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 
-              'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 
-              'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 
-              'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 
-              'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WI', 'WV', 'WY', 
-              'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 
-              'Colorado', 'Connecticut', 'District of Columbia', 'Delaware', 'Florida', 
-              'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 
-              'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 
-              'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 
-              'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 
-              'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 
-              'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 
-              'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 
-              'Vermont', 'Virginia', 'Washington', 'Wisconsin', 'West Virginia', 'Wyoming' 
-              'carolina', 'columbia', 'dakota', 'hampshire', 'mexico', 'rhode', 'york']
-    for state in states:
-        stop_word_list.append(state)
-    for state in [state.lower() for state in states]:
-        stop_word_list.append(state)
         
     # Add even more stop words:
     if extend_stopwords == True:
         stop_word_list = text.ENGLISH_STOP_WORDS.union(stop_word_list)
-        
-    # If path to old vocab not specified, skip last step and return stop word list thus far
-    if vocab_path_old == "":
-        return stop_word_list
-
-    # Add to stopwords useless and hard-to-formalize words/chars from first chunk of previous model vocab (e.g., a3d0, \fs19)
-    # First create whitelist of useful terms probably in that list, explicitly exclude from junk words list both these and words with underscores (common phrases)
-    whitelist = ["Pre-K", "pre-k", "pre-K", "preK", "prek", 
-                 "1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th", 
-                 "1st-grade", "2nd-grade", "3rd-grade", "4th-grade", "5th-grade", "6th-grade", 
-                 "7th-grade", "8th-grade", "9th-grade", "10th-grade", "11th-grade", "12th-grade", 
-                 "1st-grader", "2nd-grader", "3rd-grader", "4th-grader", "5th-grader", "6th-grader", 
-                 "7th-grader", "8th-grader", "9th-grader", "10th-grader", "11th-grader", "12th-grader", 
-                 "1stgrade", "2ndgrade", "3rdgrade", "4thgrade", "5thgrade", "6thgrade", 
-                 "7thgrade", "8thgrade", "9thgrade", "10thgrade", "11thgrade", "12thgrade", 
-                 "1stgrader", "2ndgrader", "3rdgrader", "4thgrader", "5thgrader", "6thgrader", 
-                 "7thgrader", "8thgrader", "9thgrader", "10thgrader", "11thgrader", "12thgrader"]
-    with open(vocab_path_old) as f: # Load vocab from previous model
-        junk_words = f.read().splitlines() 
-    
-    if school_whitelist == True:
-        junk_words = [word for word in junk_words[:8511] if ((not "_" in word) 
-                                                         and (not any(term in word for term in whitelist)))]
-        stop_word_list.extend(junk_words)                                      
-        return stop_word_list
-        
-    junk_words = [word for word in junk_words[:8511] if ((not "_" in word))]
-    stop_word_list.extend(junk_words)
                   
     return stop_word_list
                                                      
@@ -144,28 +90,6 @@ def unicode_make():
     return unicode_list
 
 
-def get_common_words(tokenized_corpus, max_percentage):
-    """Discover most common words in corpus up to max_percentage.
-    
-    Args:
-        Corpus tokenized by words,
-        Highest allowable frequency of documents in which a token may appear (e.g., 1-5%)
-        
-    Returns:
-        List of most frequent words in corpus"""
-    
-    # Code goes here
-    # Probably using nltk.CountVectorizer
-
-    
-# Create useful lists using above functions:
-stop_words_list = stopwords_make()
-punctstr = punctstr_make()
-unicode_list = unicode_make()
-
-# Instantiate spacy object for part of speech tagging (to remove proper nouns)
-#nlp = spacy.load('en', disable=['ner']) # For speed, keep only POS tagging functionality and parsing (which improves accuracy). Does including NER also improve accuracy? 
-
 def gather_propernouns(sentence):
     """ Creates a list of the propernouns in the sentence.
     Args:
@@ -188,122 +112,53 @@ def gather_propernouns(sentence):
 #     return new_text
 
 
+# Initialize Lucene for Apache tokenizer
+lucene.initVM(vmargs=['-Djava.awt.headless=true'])
 
-
-def clean_sentence(sentence, remove_stopwords = True, stopwords_jstor = False, remove_numbers = True, keep_english = False, slow_webclean = False, exclude_words = [], stemming = False, remove_acronyms = True, remove_propernouns = True, unhyphenate = False, return_string = False):
-    """Removes numbers, emails, URLs, unicode characters, hex characters, and punctuation from a sentence 
-    separated by whitespaces. Returns a tokenized, cleaned list of words from the sentence.
+def apache_tokenize(sentence, 
+                    lowercase = True):
+    '''
+    Tokenizes sentences into words using the Apache Lucene Standard Tokenizer (same as JSTOR).
     
-    Args: 
-        sentence, i.e. string that possibly includes spaces and punctuation
-        remove_stopwords: whether to remove stopwords, default True
-        remove_numbers: whether to remove any chars that are digits, default True
-        keep_english: whether to remove words not in english dictionary, default False; if 'restrictive', keep word only if in NLTK's dictionary of 237K english words; if 'permissive', keep word only if in longer list of 436K english words
-        slow_webclean: whether to include advanced sentence cleaning: removing emails, URLs, and unicode and hex chars, default False
-        exclude_words: list of words to exclude, may be most common words or named entities, default empty list
-        stemming: whether to apply PorterStemmer to each word, default False
-        remove_propernouns: boolean, removes nouns such as names, etc., default True 
-        unhyphenate: join any lingering hyphens (i.e., words ending with '- ') as for JSTOR text data, default False
-        return_string: return string instead of list of tokens (useful for infersent), default False
-    Returns: 
-        Cleaned & tokenized sentence, i.e. a list of cleaned, lower-case, one-word strings"""
-    
-    global stop_words_list, punctstr, unicode_list, english_nltk, nlp #, english_long
-
-    # Define stopwords used by JSTOR and implement if called:
-    jstor_list_words = set(["a", "an", "and", "are", "as", "at", "be", "but", "by", "for", "if", "in", "into", "is", "it", "no", "not", "of", "on", "or", "such", "that", "the", "their", "then", "there", "these", "they", "this", "to", "was", "will", "with"]) #jstor removal words
-    if stopwords_jstor:
-        stop_words_list = jstor_list_words # Use same stopwords as JSTOR
-    
-    # Replace unicode spaces, tabs, and underscores with spaces, and remove whitespaces from start/end of sentence:
-    sentence = sentence.replace(u"\xa0", u" ").replace(u"\\t", u" ").replace(u"_", u" ").strip(" ")
-    
-    if unhyphenate:              
-        ls = re.findall(r"\w+-\s\w+", sentence)
-        if len(ls) > 0:
-            ls_new = [re.sub(r"- ", "", word) for word in ls]
-            for i in range(len(ls)):
-                sentence= sentence.replace(ls[i], ls_new[i])
-
-    if remove_acronyms:
-        ls = re.findall(r"\b[A-Z][A-Z]+\b\s+", sentence)
-        if len(ls) > 0:
-            ls_new = np.repeat("", len(ls))
-            for i in range(len(ls)):
-                sentence = sentence.replace(ls[i], ls_new[i])
-    
-    if slow_webclean:
-        # Remove hex characters (e.g., \xa0\, \x80):
-        sentence = re.sub(r'[^\x00-\x7f]', r'', sentence) #replace anything that starts with a hex character 
-
-        # Replace \\x, \\u, \\b, or anything that ends with \u2605
-        sentence = re.sub(r"\\x.*|\\u.*|\\b.*|\u2605$", "", sentence)
-
-        # Remove all elements that appear in unicode_list (looks like r'u1000|u10001|'):
-        sentence = re.sub(r'|'.join(map(re.escape, unicode_list)), '', sentence)
-    
-    # If True, include the proper nouns in stop_words_list
-    if remove_propernouns:              
-#         doc = nlp(sentence) # Create a document object in spacy
-#         proper_nouns = gather_propernouns(doc) # Creates a wordbank of proper nouns we should exclude
-           
-        #trying to gather proper nouns by passing in pure sentence in gather_propernouns
-        proper_nouns = gather_propernouns(sentence)
-        print(proper_nouns)
-        # Remove each proper noun from sentence:
-        sentence = " ".join([word for word in sentence.split() if word not in proper_nouns])
-        #for term in proper_nouns: # Loop over wordbank
-        #    sentence = re.sub(term, "", sentence) # Less effective because removes characters from within terms
-
-    
-    sent_list = [] # Initialize empty list to hold tokenized sentence (words added one at a time)
-    
-    for word in sentence.split(): # Split by spaces and iterate over words
+    Args:
+        sentence: str
+        lowercase: binary indicator: whether to lowercase each word
+    Returns:
+        list of str: each element of list is a word
         
-        word = word.strip() # Remove leading and trailing spaces
-        
-        if remove_numbers:
-            word = re.sub(r"[0-9]+", "", word) # filter out digits
-        
-        # Filter out emails and URLs:
-        if slow_webclean and ("@" in word or word.startswith(('http', 'https', 'www', '//', '\\', 'x_', 'x/', 'srcimage')) or word.endswith(('.com', '.net', '.gov', '.org', '.jpg', '.pdf', 'png', 'jpeg', 'php'))):
-            continue
+    Requires these packages: 
+    lucene
+    org.apache.lucene.analysis.standard.StandardAnalyzer
+    org.apache.lucene.analysis.standard.StandardTokenizer
+    java.io.StringReader
+    org.apache.lucene.analysis.tokenattributes.CharTermAttribute
+    '''
+    
+    sent_list = [] # initialize empty list to add words to
+    
+    tokenizer = StandardTokenizer() # start Tokenizer
+    tokenizer.setReader(StringReader(sentence))
+    charTermAttrib = tokenizer.getAttribute(CharTermAttribute.class_)
+    tokenizer.reset()
+    
+    if lowercase:
+        while tokenizer.incrementToken():
+            sent_list.append(charTermAttrib.toString().lower()) #lowercasing
             
-        # Remove punctuation (only after URLs removed):
-        word = re.sub(r"["+punctstr+"]+", r'', word).strip("'").strip("-") # Remove punctuations, and remove dashes and apostrophes only from start/end of words
-        
-        if remove_stopwords and word in stop_words_list: # Filter out stop words
-            continue
-                            
-        # TO DO: Pass in most_common_words to function; write function to find the top 1-5% most frequent words, which we will exclude
-        # Remove most common words:
-        if word in exclude_words:
-            continue
-            
-        if keep_english == 'restrictive':
-            if word not in english_nltk: #Filter out non-English words using shorter list
-                continue
-            
-        if keep_english == 'permissive': 
-            if word not in english_long: #Filter out non-English words using longer list
-                continue
-        
-        # Stem word (if applicable):
-        if stemming:
-            word = ps.stem(word)
-        
-        sent_list.append(word.lower()) # Add lower-cased word to list (after passing checks)
+        return sent_list
     
-    if return_string:
-        return ' '.join(sent_list) # Return clean, tokenized sentence (string)
-    
-    return sent_list # Return clean, tokenized sentence (list of tokens)
-
+    # if not lower-casing:
+    while tokenizer.incrementToken():
+        sent_list.append(charTermAttrib.toString())
+        
+    return sent_list
+                         
 
 
 def clean_sentence_apache(sentence, 
                           unhyphenate = True, 
-                          remove_numbers = True, 
+                          lowercase = True, 
+                          remove_numbers = False, 
                           remove_acronyms = False, 
                           remove_stopwords = True, 
                           remove_propernouns = False, 
@@ -319,11 +174,20 @@ def clean_sentence_apache(sentence,
     and proper nouns (the last not by default).
     
     Args:
-        sentence: in str format
-        
+        sentence (str): sentence that possibly includes spaces and punctuation
+        unhyphenate (binary): whether to join any lingering hyphens at end of line (i.e., words ending with '- ')
+        lowercase (binary): whether to lower-case each word
+        remove_numbers (binary): whether to remove any chars that are digits
+        remove_acronyms (binary): whether t
+        remove_stopwords (binary): whether to remove stopwords
+        remove_propernouns (binary): boolean, removes nouns such as names, etc.
+        return_string (binary): return string instead of list of tokens (useful for infersent)  
+
     Returns:
         list of str: each element of list is a word
     '''
+     # Define JSTOR words to remove ("stop words")
+    jstor_stop_words = set(["a", "an", "and", "are", "as", "at", "be", "but", "by", "for", "if", "in", "into", "is", "it", "no", "not", "of", "on", "or", "such", "that", "the", "their", "then", "there", "these", "they", "this", "to", "was", "will", "with"]) 
     
     # Replace unicode spaces, tabs, and underscores with spaces, and remove whitespaces from start/end of sentence:
     sentence = sentence.encode('utf-8').decode('utf-8').replace(u"\xa0", u" ").replace(u"\\t", u" ").replace(u"_", u" ").strip(" ")
@@ -344,10 +208,9 @@ def clean_sentence_apache(sentence,
     
     if remove_acronyms:
         sentence = re.sub(r"\b[A-Z][A-Z]+\b\s+", "", sentence)
-
-    sent_list = []
     
     #tokenizing
+    sent_list = apache_tokenize(sent, lowercase = lowercase)
     tokenizer = StandardTokenizer()
     tokenizer.setReader(StringReader(sentence))
     charTermAttrib = tokenizer.getAttribute(CharTermAttribute.class_)
@@ -356,7 +219,6 @@ def clean_sentence_apache(sentence,
         sent_list.append(charTermAttrib.toString().lower()) #lowercasing
         
     # Remove same stopwords as JSTOR, also junk formatting words
-    jstor_stop_words = set(["a", "an", "and", "are", "as", "at", "be", "but", "by", "for", "if", "in", "into", "is", "it", "no", "not", "of", "on", "or", "such", "that", "the", "their", "then", "there", "these", "they", "this", "to", "was", "will", "with"]) #define jstor removal words
     if remove_stopwords:
         sent_list = [word for word in sent_list if 
                      word not in jstor_stop_words 
@@ -395,4 +257,3 @@ def clean_sentence_apache(sentence,
         return ' '.join(sent_list) # Return clean, tokenized sentence (string)
     
     return sent_list
-    
